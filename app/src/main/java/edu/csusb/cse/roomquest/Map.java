7b30f0fc65bb8ad3b50efbcabfe70295ea3fcd25
michaellmonaghan <michaellmonaghan@gmail.com>
2015-02-19 02:01:26 -0800

    Added core classes

--- app/src/main/java/edu/csusb/cse/roomquest/Map.java
+++ app/src/main/java/edu/csusb/cse/roomquest/Map.java
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-package edu.csusb.cse.roomquest;
-
-import java.io.File;
-
-/**
- * Created by Michael on 2/14/2015.
- */
-public interface Map {
-    File getImage(int floor);
-}
-=======
 package edu.csusb.cse.roomquest;
 
 /**
@@ -17,4 +5,3 @@ package edu.csusb.cse.roomquest;
  */
 public class Map {
 }
->>>>>>> 5a3ccf9491a0d96d1ab8ce9a3206029ecffa5b06
