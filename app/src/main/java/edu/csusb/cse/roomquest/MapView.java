fec3db92a2464710c8bd1d8cc4865f9d34884fb6
Michael Monaghan <michaellmonaghan@gmail.com>
2015-02-19 01:40:46 -0800

    merged everything

--- app/src/main/java/edu/csusb/cse/roomquest/MapView.java
+++ app/src/main/java/edu/csusb/cse/roomquest/MapView.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package edu.csusb.cse.roomquest;
 
 import android.content.Context;
@@ -21,12 +20,3 @@ public class MapView extends View {
     protected void onDraw(Canvas canvas) {
     }
 }
-=======
-package edu.csusb.cse.roomquest;
-
-/**
- * Created by Michael on 2/18/2015.
- */
-public class MapView {
-}
->>>>>>> 5a3ccf9491a0d96d1ab8ce9a3206029ecffa5b06
