d54b976dd85dd59e85dc60bb7e88ec6828569020
Michael Monaghan <michaellmonaghan@gmail.com>
2015-03-22 17:10:07 -0700

    Fixed a few syntax errors

--- app/src/main/java/edu/csusb/cse/roomquest/mapping/Hotspot.java
+++ app/src/main/java/edu/csusb/cse/roomquest/mapping/Hotspot.java
@@ -1,34 +1,41 @@
 package edu.csusb.cse.roomquest.mapping;
 
 public class Hotspot {
-	int x;
-	int y;
-	String BSSID;
-	String Power;
-	
-	public int getXLocation() {
-		return x;
-	}
-	public void setXLocation(int X) {
-		this.x = X;
-	}
-	public int getYLocation() {
-		return y;
-	}
-	public void setYLocation(int Y) {
-		this.y = Y;
-	}
+    int x;
+    int y;
+    String BSSID;
+    String Power;
+
+    public int getXLocation() {
+        return x;
+    }
+
+    public void setXLocation(int X) {
+        this.x = X;
+    }
+
+    public int getYLocation() {
+        return y;
+    }
+
+    public void setYLocation(int Y) {
+        this.y = Y;
+    }
+
+    public String getBSSID() {
+        return BSSID;
+    }
+
+    public void setBSSID(String BSSID) {
+        this.BSSID = BSSID;
+    }
+
+    public String getPower() {
+        return Power;
+    }
+
+    public void setPower1(String Power) {
+        this.Power = Power;
+    }
 }
-	public String getBSSID() {
-		return BSSID;
-	}
-	public void setBSSID(String BSSID) {
-		this.BSSID = BSSID;
-	}
-	public String getPower() {
-		return Power;
-	}
-	public void setPower1(String Power) {
-		this.Power = Power;
-	}
 	
