c4ab9132d199ae2819ed5b13bde13e3aa53f08e7
Patricio Torres <isawnconquered@users.noreply.github.com>
2015-03-22 17:04:20 -0700

    Update WifiParsing.java

--- app/src/main/java/edu/csusb/cse/roomquest/parsing/WifiParsing.java
+++ app/src/main/java/edu/csusb/cse/roomquest/parsing/WifiParsing.java
@@ -1,3 +1,14 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.csusb.cse.roomquest.mapping.Hotspot;
+
 public Wifi parseWifiFile(String file) {
 		try {
 			return parseWifiFile(new FileInputStream(file));
