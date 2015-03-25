547334b848cf8f27d91c973ed8951fa3a0f473ae
Michael Monaghan <michaellmonaghan@gmail.com>
2015-03-25 16:24:13 -0700

    Location methods created

--- app/src/main/java/edu/csusb/cse/roomquest/wifi_nav/LocationSample.java
+++ app/src/main/java/edu/csusb/cse/roomquest/wifi_nav/LocationSample.java
@@ -1,25 +1,27 @@
 package edu.csusb.cse.roomquest.wifi_nav;
 
+import java.util.Arrays;
+
 import edu.csusb.cse.roomquest.mapping.Floor;
 
 /**
  * Created by Michael on 3/24/2015.
- * Represents a HotspotSignal sample at an x y coordinate
+ * Represents all the HotspotSignals sampled at an x y coordinate and floor.
  */
 public class LocationSample {
     public final float x,y;
-    public final HotspotSignal hotspotSignal;
+    public final HotspotSignal[] hotspotSignals;
     public final Floor floor;
 
-    public LocationSample(float x, float y, Floor floor, HotspotSignal hotspotSignal) {
+    public LocationSample(float x, float y, Floor floor, HotspotSignal[] hotspotSignals) {
         this.x = x;
         this.y = y;
         this.floor = floor;
-        this.hotspotSignal = hotspotSignal;
+        this.hotspotSignals = hotspotSignals;
     }
 
     @Override
     public String toString() {
-        return hotspotSignal + " (" + x + "," + y + ")";
+        return Arrays.toString(hotspotSignals) + " (" + x + "," + y + ")";
     }
 }
